3dc54ff34557ae107f943c5640ebf40ddccc7762
primetime43 <krotec2@gmail.com>
2021-12-11 17:46:14 -0500

    Removed the check for if TTR is running.

--- [Source] Toontown Rewritten Bot/ToonTown Rewritten Bot/Form1.cs
+++ [Source] Toontown Rewritten Bot/ToonTown Rewritten Bot/Form1.cs
@@ -14,7 +14,7 @@ namespace ToonTown_Rewritten_Bot
 
         public Form1()
         {
-            isTTRRunning();
+            //isTTRRunning();
             InitializeComponent();
             BotFunctions.readTextFile();
             createDataFileMap();
