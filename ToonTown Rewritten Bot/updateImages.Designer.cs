d6681308defa477284e62e7740503527165a7671
Mike <12754111+primetime43@users.noreply.github.com>
2023-03-03 18:10:24 -0500

    Dev 2023 to master branch - v1.4 (#13)
    * Fixed the layout of the project directory
    Fixed string looking for ttr window when it was beta
    Added color recognition for red fishing button
    
    * Fixed issue where InputSimulator.dll was missing from source code
    
    * Updated the sln to target VS 2022 instead of VS 17
    
    * Upgraded the project to target .NET 7
    
    * Added the ability to create the coordinates file if it doesn't exist when trying to reset the coordinates
    
    * Update .gitignore
    Updating .gitignore to ignore the obj folder.
    
    * Added more files to the .gitignore file
    
    * Added an auto detect red fishing button toggle
    
    * Removed and cleaned up some code
    
    * Added a note on the golf tab to inform how to use
    
    ---------

---
+++ ToonTown Rewritten Bot/updateImages.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace ToonTown_Rewritten_Bot
+{
+    partial class UpdateImages
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdateImages));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fishingSellImg = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fishingExitImg = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fishingCastImg = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.fishingSellImg);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.fishingExitImg);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.fishingCastImg);
+            this.groupBox1.Location = new System.Drawing.Point(12, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(499, 107);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Fishing Images";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Sell Button:";
+            // 
+            // fishingSellImg
+            // 
+            this.fishingSellImg.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.fishingSellImg.Location = new System.Drawing.Point(103, 71);
+            this.fishingSellImg.Name = "fishingSellImg";
+            this.fishingSellImg.ReadOnly = true;
+            this.fishingSellImg.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.fishingSellImg.Size = new System.Drawing.Size(379, 20);
+            this.fishingSellImg.TabIndex = 4;
+            this.fishingSellImg.Text = "Click to set image";
+            this.fishingSellImg.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.fishingSellImg.Click += new System.EventHandler(this.fishingSellImg_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Exit Button:";
+            // 
+            // fishingExitImg
+            // 
+            this.fishingExitImg.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.fishingExitImg.Location = new System.Drawing.Point(103, 45);
+            this.fishingExitImg.Name = "fishingExitImg";
+            this.fishingExitImg.ReadOnly = true;
+            this.fishingExitImg.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.fishingExitImg.Size = new System.Drawing.Size(379, 20);
+            this.fishingExitImg.TabIndex = 2;
+            this.fishingExitImg.Text = "Click to set image";
+            this.fishingExitImg.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.fishingExitImg.Click += new System.EventHandler(this.fishingExitImg_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cast Button:";
+            // 
+            // fishingCastImg
+            // 
+            this.fishingCastImg.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.fishingCastImg.Location = new System.Drawing.Point(103, 19);
+            this.fishingCastImg.Name = "fishingCastImg";
+            this.fishingCastImg.ReadOnly = true;
+            this.fishingCastImg.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.fishingCastImg.Size = new System.Drawing.Size(379, 20);
+            this.fishingCastImg.TabIndex = 0;
+            this.fishingCastImg.Text = "Click to set image";
+            this.fishingCastImg.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.fishingCastImg.Click += new System.EventHandler(this.fishingCastImg_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(517, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(271, 163);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(592, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Click the image to update it";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(129, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(326, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Click a textbox below to view or set the image if the textbox is empty";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(672, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 28);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Advanced Settings";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // UpdateImages
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "UpdateImages";
+            this.Text = "Image Recognition Settings";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox fishingCastImg;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox fishingSellImg;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox fishingExitImg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ ToonTown Rewritten Bot/updateImages.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ToonTown_Rewritten_Bot
+{
+    public partial class UpdateImages : Form
+    {
+        private static ToolTip tip = new ToolTip();
+        private static string currentSettingName = "", currentSettingValue = "";
+        private static Dictionary<string, TextBox> dataMap = new Dictionary<string, TextBox>();
+        public UpdateImages()
+        {
+            InitializeComponent();
+            createDataMap();
+            updateTextBoxes();
+        }
+
+        //settingName,txtboxObj
+        private void createDataMap()
+        {
+            //fishing
+            dataMap["fishingCastBtn"] = fishingCastImg;//do this instead of add method so it will just overwrite
+            dataMap["exitFishingBtn"] = fishingExitImg;
+            dataMap["sellFishBtn"] = fishingSellImg;
+            //
+        }
+
+        private void updateTextBoxes()
+        {
+            foreach (SettingsProperty currentProperty in Properties.Settings.Default.Properties)
+            {
+                if (Properties.Settings.Default[currentProperty.Name].ToString() != "")//the value of the setting name doesnt equal null
+                {
+                    dataMap[currentProperty.Name].Text = Properties.Settings.Default[currentProperty.Name].ToString();
+                }
+                else
+                    Console.WriteLine("Value is null");
+            }
+        }
+
+        private void fishingCastImg_Click(object sender, EventArgs e)
+        {
+            currentSettingName = "fishingCastBtn";
+            currentSettingValue = Properties.Settings.Default[currentSettingName].ToString();
+            if (currentSettingValue == "")//has no path set, so set path
+            {
+                updateImage();
+                fishingCastImg.Text = currentSettingValue;
+            }
+            else
+                pictureBox1.Image = Image.FromFile(currentSettingValue);
+        }
+
+        private void fishingExitImg_Click(object sender, EventArgs e)
+        {
+            currentSettingName = "exitFishingBtn";
+            currentSettingValue = Properties.Settings.Default[currentSettingName].ToString();
+            if (currentSettingValue == "")//has no path set, so set path
+            {
+                updateImage();
+                fishingExitImg.Text = currentSettingValue;
+            }
+            else
+                pictureBox1.Image = Image.FromFile(currentSettingValue);
+        }
+
+        private void fishingSellImg_Click(object sender, EventArgs e)
+        {
+            currentSettingName = "sellFishBtn";
+            currentSettingValue = Properties.Settings.Default[currentSettingName].ToString();
+            if (currentSettingValue == "")//has no path set, so set path
+            {
+                updateImage();
+                fishingSellImg.Text = currentSettingValue;
+            }
+            else
+                pictureBox1.Image = Image.FromFile(currentSettingValue);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            updateImage();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                new AdvancedSettings().ShowDialog();
+            }
+            catch
+            {
+                MessageBox.Show("Unable to perform this action", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+        }
+
+        private void updateImage()
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                string filePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @".\Search_Images"));
+                openFileDialog.InitialDirectory = filePath;
+                openFileDialog.Filter = "PNG files (*.png)|*.png";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Properties.Settings.Default[currentSettingName] = openFileDialog.FileName;
+                    Properties.Settings.Default.Save();
+                    pictureBox1.Image = Image.FromFile(Properties.Settings.Default[currentSettingName].ToString());
+                    currentSettingValue = Properties.Settings.Default[currentSettingName].ToString();
+                    updateTextBoxes();
+                }
+            }
+        }
+    }
+}
