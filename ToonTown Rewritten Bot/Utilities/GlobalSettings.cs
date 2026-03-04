73cb9a1def69561cc89869af6e2b2d0c20f753d4
primetime43 <12754111+primetime43@users.noreply.github.com>
2026-03-04 07:44:28 -0500

    Bump version to 2.2.0 across project files
    Updated AssemblyInfo.cs, project file, and GlobalSettings.cs to reflect new application version 2.2.0.

--- ToonTown Rewritten Bot/Utilities/GlobalSettings.cs
+++ ToonTown Rewritten Bot/Utilities/GlobalSettings.cs
@@ -4,7 +4,7 @@
     {
         public static class ApplicationInfo
         {
-            public static string Version { get; } = "2.1.1";
+            public static string Version { get; } = "2.2.0";
             public static string Author { get; } = "primetime43";
         }
     }
